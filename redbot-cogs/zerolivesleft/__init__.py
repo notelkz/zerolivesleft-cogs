7a03e4c5b4bf9b87f9f21c834e321f216cec1b46
notelkz <79387215+notelkz@users.noreply.github.com>
2025-05-18 11:01:30 +0100

    Delete redbot-cogs directory

--- redbot-cogs/zerolivesleft/__init__.py
+++
@@ -1,8 +0,0 @@
-from .zerolivesleft import ZeroLivesLeft
-from .wp_reports import WPReports
-
-async def setup(bot):
-    cog = ZeroLivesLeft(bot)
-    await bot.add_cog(cog)
-    cog = WPReports(bot)
-    await bot.add_cog(cog)
